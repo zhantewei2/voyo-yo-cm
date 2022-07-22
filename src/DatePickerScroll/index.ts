cebe6951f99b94d8b7397af2f2d8582d5c56a5cd
zhan <zhantewei@gmail.com>
2022-07-22 18:42:26 +0800

    debug

--- src/DatePickerScroll/index.ts
+++ src/DatePickerScroll/index.ts
@@ -1,12 +1,7 @@
+import Vue from "vue";
 import {Module} from "../util";
 import DatePickerScrollItem from "./datepicker-scoll-item.vue";
 import DatePickerScroll from "./datepicker-scroll.vue";
-
-
-export {
-  DatePickerScrollItem,
-  DatePickerScroll
-}
 
 export const DatePickerScrollModule=Module("datepicker-scroll",(vue)=>{
   vue.component("yo-datepicker-scroll",DatePickerScroll);
--- types/DatePickerScroll/index.d.ts
+++ types/DatePickerScroll/index.d.ts
@@ -1,4 +1,1 @@
-import DatePickerScrollItem from "./datepicker-scoll-item.vue";
-import DatePickerScroll from "./datepicker-scroll.vue";
-export { DatePickerScrollItem, DatePickerScroll };
 export declare const DatePickerScrollModule: import("..").ModuleReturn;
